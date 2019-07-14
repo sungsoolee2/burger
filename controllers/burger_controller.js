122afe94e412b45dd61fd8f16ce03f28612d5d45
Sung Lee <sungsoolee2@hotmail.com>
2019-07-14 14:06:43 -0500

    Second Draft

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,28 +3,27 @@ var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
 
+//Routes
 
-// Create routes
-// ----------------------------------------------------
-// Index Redirect
+// Redirect
 router.get('/', function (req, res) {
   res.redirect('/index');
 });
 
 
-// Index Page (render all burgers to DOM)
+// Index 
 router.get('/index', function (req, res) {
-  burger.selectAll(function(data) {
-    var hbsObject = { burgers: data };
-    //console.log(hbsObject);
-    res.render('index', hbsObject);
+  burger.all(function(data) {
+    var BurgerData = { burgers: data };
+    console.log(BurgerData);
+    res.render('index', BurgerData);
   });
 });
 
 
 // Create a New Burger
 router.post('/burger/create', function (req, res) {
-  burger.insertOne(req.body.burger_name, function() {
+  burger.insertBurger(req.body.burger_name, function() {
     res.redirect('/index');
   });
 });
@@ -32,7 +31,7 @@ router.post('/burger/create', function (req, res) {
 
 // Devour a Burger
 router.post('/burger/eat/:id', function (req, res) {
-  burger.updateOne(req.params.id, function() {
+  burger.updateBurger(req.params.id, function() {
     res.redirect('/index');
   });
 });
