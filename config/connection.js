35c2ce220a032f804101769b679002074870682b
Sung Lee <sungsoolee2@hotmail.com>
2019-07-15 10:59:27 -0500

    JASWDB add

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,9 @@ var connection;
 
 if (process.env.JAWSDB_URL){
 connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{host: "localhost",
-  port: 3306,
+} else{
+connection =mysql.createConnection({
+  host: "localhost",
   user: "root",
   password: "root",
   database: "burger"
