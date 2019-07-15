66a9ce1ec5d258221a329b1ebe756a7364ae06d1
Sung Lee <sungsoolee2@hotmail.com>
2019-07-15 10:47:32 -0500

    JAWSDB add

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+var connection;
+If (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "burger"
-});
+  });
+  
+};
 
 // Make connection.
 connection.connect(function(err) {
