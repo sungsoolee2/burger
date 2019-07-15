6b967333d888db32589910e08b8abdba0206a1e7
Sung Lee <sungsoolee2@hotmail.com>
2019-07-15 10:17:59 -0500

    Add env

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+//Process.env
+var PORT =process.env.PORT || 8080;
+
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
